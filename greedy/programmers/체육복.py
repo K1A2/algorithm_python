1bef5042b37f0fcb86b0614ee5570e519a07252c
K1A2 <jckim0307@gmail.com>
2021-09-13 23:44:40 +0900

    edit greedy

--- greedy/programmers/체육복.py
+++ greedy/programmers/체육복.py
@@ -1,38 +1,41 @@
+def reserve_check(l, check):
+    if 0 <= l < len(check):
+        if not check[l]:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
 def solution(n, lost, reserve):
     cloth = [1 for _ in range(n)]
     check = [False for _ in range(n)]
     for i in range(n):
         if i + 1 in lost:
             cloth[i] -= 1
-        elif i + 1 in reserve:
+        if i + 1 in reserve:
             cloth[i] += 1
-
-    for i in range(n):
-        count = cloth[i]
-        if count == 1:
+        if cloth[i] > 0:
             check[i] = True
-        elif count == 2:
-            prev = i - 1
-            next = i + 1
-            if prev >= 0 and not check[prev]:
-                if cloth[prev] == 0:
-                    cloth[prev] += 1
-                    cloth[i] -= 1
-                    check[prev] = True
-                else:
-                    if next < len(check) and not check[next]:
-                        if cloth[next] == 0:
-                            cloth[next] += 1
-                            cloth[i] -= 1
-                        check[next] = True
-            elif next < len(check) and not check[next]:
-                if cloth[next] == 0:
+        else:
+            check[i] = False
+
+    for i in range(len(cloth)):
+        if cloth[i] == 2:
+            prev, next = i - 1, i + 1
+            if reserve_check(prev, check):
+                cloth[prev] += 1
+                cloth[i] -= 1
+                check[prev] = True
+            else:
+                if reserve_check(next, check):
                     cloth[next] += 1
                     cloth[i] -= 1
-                check[next] = True
-            check[i] = True
-    return len(cloth) - cloth.count(0)
+                    check[next] = True
 
-print(solution(5, [2, 4], [1, 3, 5])) # 5
-print(solution(5, [2, 4], [3])) # 4
-print(solution(3, [3], [1])) # 2
+    return check.count(True)
+
+print(solution(5, [2, 4], [1, 3, 5]) == 5) # 5
+print(solution(5, [2, 4], [3]) == 4) # 4
+print(solution(3, [3], [1]) == 2) # 2
