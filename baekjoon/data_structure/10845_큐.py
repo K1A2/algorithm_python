1571452673a18c7c81b9b79e00c35b66d919e952
K1A2 <jckim0307@gmail.com>
2022-01-11 22:18:11 +0900

    백준 10845 풀이

---
+++ baekjoon/data_structure/10845_큐.py
@@ -0,0 +1,29 @@
+from collections import deque
+import sys
+q = deque()
+for _ in range(int(sys.stdin.readline().rstrip())):
+    com = sys.stdin.readline().rstrip().split()
+    if com[0] == 'push':
+        q.append(int(com[1]))
+    elif com[0] == 'pop':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q.popleft())
+    elif com[0] == 'size':
+        print(len(q))
+    elif com[0] == 'empty':
+        if len(q) == 0:
+            print(1)
+        else:
+            print(0)
+    elif com[0] == 'front':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[0])
+    elif com[0] == 'back':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[-1])
