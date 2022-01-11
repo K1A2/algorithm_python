00f841c491fc284c4044f75860440da51383537e
K1A2 <jckim0307@gmail.com>
2022-01-11 12:42:07 +0900

    백준 2206 재체점 코드 수정

--- baekjoon/bfs_dfs/2206_벽부수고_이동하기.py
+++ baekjoon/bfs_dfs/2206_벽부수고_이동하기.py
@@ -2,21 +2,16 @@ from collections import deque
 from sys import stdin
 n, m = map(int, stdin.readline().split())
 data = [[[-1, 1, i] for i in list(map(int, stdin.readline().rstrip()))] for _ in range(n)] # visited, count, type
-dxy = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+dxy = ((-1, 0), (1, 0), (0, -1), (0, 1))
 
 queue = deque()
-queue.append((0, 0, 0, 2))
 save = deque()
+queue.append((0, 0, 0, 2))
 while queue:
     x, y, wall_breaked, index = queue.popleft()
     if x == n - 1 and y == m - 1:
         print(data[x][y][1])
         exit()
-    if data[x][y][0] == index:
-        if not queue and save:
-            queue.append(save.popleft())
-        continue
-    data[x][y][0] = index
     for d in dxy:
         nx = x + d[0]
         ny = y + d[1]
@@ -24,18 +19,26 @@ while queue:
             continue
         else:
             if data[nx][ny][2] == 1:
-                if not wall_breaked: # 분순적 없는 벽
+                if not wall_breaked: # 부순적 없는 벽
                     data[nx][ny][1] = data[x][y][1] + 1
                     data[nx][ny][2] = 2
-                    queue.append((nx, ny, 1, index * -1))
-                    save.append((x, y, 0, index))
+                    if data[nx][ny][0] != index * -1:
+                        queue.append((nx, ny, 1, index * -1))
+                        data[nx][ny][0] = index * -1
+                        save.append((x, y, 0, index))
             elif data[nx][ny][2] == 0:
                 if index < 0 < data[nx][ny][0]:
                     if not queue and save:
-                        queue.append(save.popleft())
+                        s = save.popleft()
+                        queue.append(s)
+                        data[s[0]][s[1]][0] = s[3]
                     continue
-                data[nx][ny][1] = data[x][y][1] + 1
-                queue.append((nx, ny, wall_breaked, index))
+                if data[nx][ny][0] != index:
+                    data[nx][ny][1] = data[x][y][1] + 1
+                    queue.append((nx, ny, wall_breaked, index))
+                    data[nx][ny][0] = index
     if not queue and save:
-        queue.append(save.popleft())
+        s = save.popleft()
+        queue.append(s)
+        data[s[0]][s[1]][0] = s[3]
 print(-1)
