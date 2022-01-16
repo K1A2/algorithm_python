8784ba97f985a3c835fef32f81241f79cb6716b5
K1A2 <jckim0307@gmail.com>
2022-01-16 12:04:53 +0900

    다익스트라 알고리즘 작성

---
+++ shortest_path/dijkstra.py
@@ -0,0 +1,36 @@
+INF = int(1e9)
+n, m = 6, 11
+start = 1
+graph = [[], [(2, 2), (3, 5), (4, 1)], [(3, 3), (4, 2)], [(2, 3), (6, 5)], [(3, 3), (5, 1)], [(3, 1), (6, 2)], []]
+visited = [False, False, False, False, False, False, False]
+distance = [1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000]
+
+def get_smallest_node():
+    min_value = INF
+    index = 0
+    for i in range(1, n + 1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
+
+def dijkstra(start):
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    for i in range(n - 1):
+        now = get_smallest_node()
+        visited[now] = True
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+    if distance[i] == INF:
+        print('INFINITY')
+    else:
+        print(distance[i])
