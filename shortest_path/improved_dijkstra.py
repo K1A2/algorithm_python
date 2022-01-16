2fa21e8c5bf7de98e9e6d575d43f60b0f85d5c23
K1A2 <jckim0307@gmail.com>
2022-01-16 12:08:43 +0900

    개선된 다익스트라 알고리즘 작성

---
+++ shortest_path/improved_dijkstra.py
@@ -0,0 +1,29 @@
+import heapq
+INF = int(1e9)
+n, m = 6, 11
+start = 1
+graph = [[], [(2, 2), (3, 5), (4, 1)], [(3, 3), (4, 2)], [(2, 3), (6, 5)], [(3, 3), (5, 1)], [(3, 1), (6, 2)], []]
+visited = [False, False, False, False, False, False, False]
+distance = [1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000]
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+    if distance[i] == INF:
+        print('INFINITY')
+    else:
+        print(distance[i])
