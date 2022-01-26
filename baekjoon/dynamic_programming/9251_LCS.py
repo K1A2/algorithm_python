9bb73945fc0539d59ee03f65dd8192e06ce135bd
K1A2 <jckim0307@gmail.com>
2022-01-26 21:45:20 +0900

    백준 10953 풀이

--- baekjoon/dynamic_programming/9251_LCS.py
+++ baekjoon/dynamic_programming/9251_LCS.py
@@ -1,7 +1,7 @@
 import sys
 a = sys.stdin.readline().rstrip()
 b = sys.stdin.readline().rstrip()
-count = [[0] * (len(a) + 1 )for _ in range(len(b) + 1)]
+count = [[0] * (len(a) + 1)for _ in range(len(b) + 1)]
 for i in range(1, len(b) + 1):
     for j in range(1, len(a) + 1):
         if a[j - 1] == b[i - 1]:
