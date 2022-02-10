d58c418ce579e3c075ca60d3a7d5c7c79426261b
K1A2 <jckim0307@gmail.com>
2022-02-11 01:56:18 +0900

    백준 23081 풀이

--- baekjoon/implementation/23081_오델로.py
+++ baekjoon/implementation/23081_오델로.py
@@ -1,30 +1,18 @@
 n = int(input())
-data = [list(input()) for _ in range(n)]
-res = [0, 0]
-check_pos = ((0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1))
+d = [list(input()) for _ in range(n)]
+r = [0]
+def count(dir):
+    nx, ny = x + dir[0], y + dir[1]
+    c = 0
+    while 0 <= nx < n and 0 <= ny < n:
+        if d[nx][ny] == '.': return 0
+        elif d[nx][ny] == 'W': c += 1
+        else: return c
+        nx, ny = nx + dir[0], ny + dir[1]
+    return 0
 for x in range(n):
     for y in range(n):
-        if data[x][y] == '.':
-            count_all = 0
-            for check_dir in check_pos:
-                nx = x + check_dir[0]
-                ny = y + check_dir[1]
-                count = 0
-                is_possible = False
-                while 0 <= nx < n and 0 <= ny < n:
-                    if data[nx][ny] == '.':
-                        break
-                    elif data[nx][ny] == 'B':
-                        is_possible = True
-                        break
-                    nx += check_dir[0]
-                    ny += check_dir[1]
-                    count += 1
-                if is_possible and count:
-                    count_all += count
-            if count_all and res[1] < count_all:
-                res = [[y, x], count_all]
-if res != [0, 0]:
-    print(' '.join([str(i) for i in res[0]]) + '\n' + str(res[1]))
-else:
-    print('PASS')
+        if d[x][y] == '.':
+            count_all = sum([count(i) for i in ((0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1))])
+            if r[0] < count_all: r = [count_all, y, x]
+print(f'{r[1]} {r[2]}\n{r[0]}') if r != [0] else print('PASS')
