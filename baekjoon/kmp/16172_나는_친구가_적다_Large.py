e998b496819c4a1e3aab42772396a2ffd577edeb
K1A2 <jckim0307@gmail.com>
2022-02-14 06:48:03 +0900

    백준 16172 풀이

---
+++ baekjoon/kmp/16172_나는_친구가_적다_Large.py
@@ -0,0 +1,31 @@
+import sys
+a = sys.stdin.readline().rstrip()
+pattern = sys.stdin.readline().rstrip()
+def get_pi():
+    m = len(pattern)
+    lps = [0] * m
+    idx = 0
+    for i in range(1, m):
+        while idx > 0 and pattern[i] != pattern[idx]:
+            idx = lps[idx - 1]
+        if pattern[i] == pattern[idx]:
+            idx += 1
+            lps[i] = idx
+    return lps
+def kmp():
+    n = len(a)
+    m = len(pattern)
+    lps = get_pi()
+    idx = 0
+    for i in range(n):
+        if 48 <= ord(a[i]) <= 57:
+            continue
+        while idx > 0 and a[i] != pattern[idx]:
+            idx = lps[idx - 1]
+        if a[i] == pattern[idx]:
+            if idx == m - 1:
+                return 1
+            else:
+                idx += 1
+    return 0
+print(kmp())
