043b0f65fd90f09c20d7d65b3097da5bacce0b51
K1A2 <jckim0307@gmail.com>
2022-03-01 00:08:41 +0900

    백준 11053 풀이

---
+++ baekjoon/dynamic_programming/11053_가장_긴_증가하는_부분수열.py
@@ -0,0 +1,9 @@
+import sys
+n = int(input())
+data = list(map(int, sys.stdin.readline().rstrip().split()))
+dp = [1] * n
+for i in range(n):
+    for j in range(i):
+        if data[i] > data[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(max(dp))
