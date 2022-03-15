6fa6f2c267c84bd624b5709ffb8dcfd484f5ad74
K1A2 <jckim0307@gmail.com>
2022-03-16 08:36:41 +0900

    백준 5086 풀이

---
+++ baekjoon/math/5086_배수와_약수.py
@@ -0,0 +1,11 @@
+import sys
+while 1:
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    if not a and not b:
+        break
+    if a % b == 0:
+        print('multiple')
+    elif b % a == 0:
+        print('factor')
+    else:
+        print('neither')
