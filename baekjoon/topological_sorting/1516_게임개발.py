1b84138af7b0fc0d358d85dd5f8ded67fca47636
K1A2 <jckim0307@gmail.com>
2022-07-01 09:21:47 +0900

    백준 1516 게임개발 풀이

---
+++ baekjoon/topological_sorting/1516_게임개발.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+n = int(input().rstrip())
+graph = [[] for _ in range(n + 1)]
+in_count = [0] * (n + 1)
+time = [0] * (n + 1)
+result = [0] * n
+for i in range(1, n + 1):
+    l = list(map(int, input().rstrip().split()))
+    time[i] = l[0]
+    result[i - 1] = l[0]
+    for j in range(1, len(l) - 1):
+        graph[l[j]].append(i)
+        in_count[i] += 1
+
+def topological_sort():
+    d = deque()
+    for i in range(1, n + 1):
+        if not in_count[i]:
+            d.append(i)
+    while d:
+        now = d.popleft()
+        for i in graph[now]:
+            in_count[i] -= 1
+            result[i - 1] = max(result[now - 1] + time[i], result[i - 1])
+            if not in_count[i]:
+                d.append(i)
+topological_sort()
+print('\n'.join(map(str, result)))
