390ad4e0f53ad5046d57dd53cb2cd6370ba3d997
K1A2 <jckim0307@gmail.com>
2022-09-27 14:30:47 +0900

    백준 13549 숨바꼭질3 풀이

---
+++ baekjoon/bfs_dfs/13549_숨바꼭질3.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+a, b = map(int, input().rstrip().split())
+
+max_len = 100000 + 1
+
+visited = [-1] * max_len
+
+d = deque()
+d.append((a, 0))
+visited[a] = 1
+
+while d:
+    pos, sec = d.popleft()
+    if pos == b:
+        print(sec)
+        break
+    for np in [pos * 2, pos + 1, pos - 1]:
+        if 0 <= np < max_len and visited[np] == -1:
+            ns = sec + (0 if pos * 2 == np else 1)
+            visited[np] = min(visited[np], ns) if visited[np] != -1 else ns
+            if pos * 2 == np:
+                d.appendleft((np, ns))
+            else:
+                d.append((np, ns))
