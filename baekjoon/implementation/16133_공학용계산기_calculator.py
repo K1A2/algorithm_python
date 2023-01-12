c782a80b5415ebfc875a77407429cb0452b31cd5
K1A2 <jckim0307@gmail.com>
2023-01-13 00:24:36 +0900

    백준 16133 공학용계산기 풀이

--- baekjoon/implementation/16133_공학용계산기_calculator.py
+++ baekjoon/implementation/16133_공학용계산기_calculator.py
@@ -1,4 +1,5 @@
 import sys
+import math
 from collections import deque
 input = sys.stdin.readline
 data = input().rstrip()
@@ -6,11 +7,74 @@ stack = deque()
 infix_data = deque()
 prev_number = ''
 prior = {'#': 5, '^': 4, '*': 3, '/': 3, '+': 2, '-': 2, '(': 1}
-for d in data:
+# prev_op = None
+# i = 0
+# while i < len(data) - 1:
+#
+for d in data[:-1]:
     if d.isdigit():
         prev_number += d
     else:
-        infix_data.append(int(prev_number))
-        prev_number = ''
-        if d == '('
-print(infix_data)
+        if prev_number != '':
+            infix_data.append(int(prev_number))
+            prev_number = ''
+        if d == '#':
+            stack.append('#')
+        elif d == '(':
+            stack.append('(')
+        elif d == ')':
+            while stack[-1] != '(':
+                infix_data.append(stack.pop())
+            stack.pop()
+            if stack and stack[-1] == '#':
+                infix_data.append(stack.pop())
+        else:
+            if d == '#' and stack and stack[-1] == '#':
+                stack.append('#')
+            elif d == '^':
+                idx = len(stack) - 1
+                flag = 1
+                while idx >= 0:
+                    if prior[stack[idx]] < prior[d]:
+                        break
+                    flag = 0
+                    idx -= 1
+                if flag:
+                    while stack and prior[d] <= prior[stack[-1]]:
+                        infix_data.append(stack.pop())
+                else:
+                    while stack and prior[d] < prior[stack[-1]]:
+                        infix_data.append(stack.pop())
+                stack.append(d)
+            else:
+                while stack and prior[d] <= prior[stack[-1]]:
+                    infix_data.append(stack.pop())
+                stack.append(d)
+if prev_number != '':
+    infix_data.append(int(prev_number))
+while stack:
+    infix_data.append(stack.pop())
+while infix_data:
+    d = infix_data.popleft()
+    if d == '#':
+        ans = math.isqrt(stack.pop())
+        if ans < 0: ans += 1
+        stack.append(ans)
+    elif d == '^':
+        a, b = stack.pop(), stack.pop()
+        stack.append(b ** a)
+    elif d == '*':
+        stack.append(stack.pop() * stack.pop())
+    elif d == '/':
+        a, b = stack.pop(), stack.pop()
+        ans = b // a
+        if ans < 0: ans += 1
+        stack.append(ans)
+    elif d == '-':
+        a, b = stack.pop(), stack.pop()
+        stack.append(b - a)
+    elif d == '+':
+        stack.append(stack.pop() + stack.pop())
+    else:
+        stack.append(d)
+print(stack.pop())
