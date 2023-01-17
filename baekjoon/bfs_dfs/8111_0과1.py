61199a8a4520b8c14d0b097c20601c3112ca1619
K1A2 <jckim0307@gmail.com>
2023-01-17 16:24:18 +0900

    백준 8111 0과1 풀이

---
+++ baekjoon/bfs_dfs/8111_0과1.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(n):
+    queue = deque()
+    visited = [0] * 20001
+    visited[1] = 1
+    queue.append((1, '1'))
+    while queue:
+        mod, num = queue.popleft()
+        if mod == 0:
+            return num
+        if len(num) > 100:
+            return 'BARK'
+        for i in range(2):
+            n_mod = (mod * 10 + i) % n
+            n_num = num + str(i)
+            if not visited[n_mod]:
+                visited[n_mod] = 1
+                queue.append((n_mod, n_num))
+    return 'BARK'
+
+for _ in range(int(input())):
+    print(bfs(int(input())))
