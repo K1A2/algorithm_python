0889b956d82981356669ab58c177b7907345c390
K1A2 <jckim0307@gmail.com>
2023-01-19 20:57:50 +0900

    백준 12015 가장 긴 증가하는 수열 2 풀이

---
+++ baekjoon/binary_search/12015_가장긴_증가하는_부분수열2.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+data = list(map(int, input().split()))
+lis = [data[0]]
+
+for i in data:
+    if lis[-1] < i:
+        lis.append(i)
+    else:
+        start = 0
+        end = len(lis) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if lis[mid] == i:
+                start = mid
+                break
+            if lis[mid] < i:
+                start = mid + 1
+            else:
+                end = mid - 1
+        lis[start] = i
+print(len(lis))
