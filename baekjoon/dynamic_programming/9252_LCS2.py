9e0291d6dbe6f04bc3aee265b370981476343ff5
K1A2 <jckim0307@gmail.com>
2023-01-23 20:53:45 +0900

    백준 9252 LCS2 풀이

--- baekjoon/dynamic_programming/9252_LCS2.py
+++ baekjoon/dynamic_programming/9252_LCS2.py
@@ -1,16 +1,15 @@
 import sys
-a = sys.stdin.readline().rstrip()
-b = sys.stdin.readline().rstrip()
-count = [['' for _ in range(len(a) + 1)] for _ in range(len(b) + 1)]
-for i in range(1, len(b) + 1):
-    for j in range(1, len(a) + 1):
-        if a[j - 1] == b[i - 1]:
-            count[i][j] = count[i - 1][j - 1] + b[i - 1]
+a = list(sys.stdin.readline().rstrip())
+b = list(sys.stdin.readline().rstrip())
+count = [[''] * (len(b) + 1) for _ in range(len(a) + 1)]
+for i in range(1, len(a) + 1):
+    for j in range(1, len(b) + 1):
+        if a[i - 1] == b[j - 1]:
+            count[i][j] = count[i - 1][j - 1] + a[i - 1]
         else:
             if len(count[i - 1][j]) < len(count[i][j - 1]):
                 count[i][j] = count[i][j - 1]
             else:
-                count[i][j] = count[i - 1][j]
                 count[i][j] = count[i - 1][j]
 print(len(count[-1][-1]))
 print(count[-1][-1])
