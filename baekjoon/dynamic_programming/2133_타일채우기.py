658f18d43a067f98f36e80d2b21c9029b92bc09a
K1A2 <jckim0307@gmail.com>
2023-01-25 10:43:00 +0900

    백준 2133 타일채우기 풀이

---
+++ baekjoon/dynamic_programming/2133_타일채우기.py
@@ -0,0 +1,10 @@
+n = int(input())
+dp = [0] * 31
+dp[0] = 1
+dp[1] = 0
+dp[2] = 3
+for i in range(4, n + 1, 2):
+    dp[i] = 3 * dp[i - 2]
+    for j in range(4, i + 1, 2):
+        dp[i] += 2 * dp[i - j]
+print(dp[n])
