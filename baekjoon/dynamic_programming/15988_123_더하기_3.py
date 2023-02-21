c07543a4492e2c92653606fa8913f8fd31028da8
K1A2 <jckim0307@gmail.com>
2023-02-22 01:01:23 +0900

    백준 15988 1,2,3 더하기 3 풀이

---
+++ baekjoon/dynamic_programming/15988_123_더하기_3.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+dp = [1, 2, 4]
+for _ in range(int(input())):
+    n = int(input())
+    for i in range(len(dp), n):
+        dp.append((dp[i - 3] + dp[i - 2] + dp[i - 1]) % 1000000009)
+    print(dp[n - 1])
