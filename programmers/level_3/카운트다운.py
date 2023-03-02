675b54a9b40651decd5b720e3e5a8b0249de1b58
K1A2 <jckim0307@gmail.com>
2023-03-02 19:48:47 +0900

    프로그래머스 카운트다운 풀이

--- programmers/level_3/카운트다운.py
+++ programmers/level_3/카운트다운.py
@@ -1,19 +1,27 @@
 def solution(target):
     dp = [[target - i, target - i] for i in range(target + 1)]
     for i in range(target, -1, -1):
-        if i - 50 >= 0:
-            if dp[i - 50][0] > dp[i][0] + 1:
-                dp[i - 50] = list(map(lambda x: x + 1, dp[i]))
-            elif dp[i - 50][0] == dp[i][0] + 1:
-                dp[i - 50] = [dp[i - 50][0] + 1, max(dp[i][1], dp[i - 50][1]) + 1]
         for j in range(20, 0, -1):
+            if i - 50 >= 0:
+                if dp[i - 50][0] > dp[i][0] + 1:
+                    dp[i - 50] = list(map(lambda x: x + 1, dp[i]))
+                elif dp[i - 50][0] == dp[i][0] + 1 and dp[i - 50][1] < dp[i][1] + 1:
+                    dp[i - 50][1] = dp[i][1] + 1
             if i - j >= 0:
                 if dp[i - j][0] > dp[i][0] + 1:
                     dp[i - j] = list(map(lambda x: x + 1, dp[i]))
-                elif dp[i - j][0] == dp[i][0] + 1:
-                    dp[i - j] = [dp[i - j][0] + 1, max(dp[i][1], dp[i - j][1]) + 1]
-                else:
-                    dp[i - j] =
+                elif dp[i - j][0] == dp[i][0] + 1 and dp[i - j][1] < dp[i][1] + 1:
+                    dp[i - j][1] = dp[i][1] + 1
+            if i - j * 2 >= 0:
+                if dp[i - j * 2][0] > dp[i][0] + 1:
+                    dp[i - j * 2] = [dp[i][0] + 1, dp[i][1]]
+                elif dp[i - j * 2][0] == dp[i][0] + 1 and dp[i - j * 2][1] < dp[i][1]:
+                    dp[i - j * 2][1] = dp[i][1]
+            if i - j * 3 >= 0:
+                if dp[i - j * 3][0] > dp[i][0] + 1:
+                    dp[i - j * 3] = [dp[i][0] + 1, dp[i][1]]
+                elif dp[i - j * 3][0] == dp[i][0] + 1 and dp[i - j * 3][1] < dp[i][1]:
+                    dp[i - j * 3][1] = dp[i][1]
     return dp[0]
 
 testcase = [
