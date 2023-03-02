5c39ba5de1b7b1e5c5c04cff0a872975bbda5134
K1A2 <jckim0307@gmail.com>
2023-03-02 17:03:33 +0900

    프로그래머스 카운트다운 중간저장

--- programmers/level_3/카운트다운.py
+++ programmers/level_3/카운트다운.py
@@ -1,14 +1,19 @@
 def solution(target):
     dp = [[target - i, target - i] for i in range(target + 1)]
     for i in range(target, -1, -1):
+        if i - 50 >= 0:
+            if dp[i - 50][0] > dp[i][0] + 1:
+                dp[i - 50] = list(map(lambda x: x + 1, dp[i]))
+            elif dp[i - 50][0] == dp[i][0] + 1:
+                dp[i - 50] = [dp[i - 50][0] + 1, max(dp[i][1], dp[i - 50][1]) + 1]
         for j in range(20, 0, -1):
-            if i - 50 >= 0:
-                if dp[i - 50][0] > dp[i][0] + 1:
-                    dp[i - 50] = list(map(lambda x: x + 1, dp[i]))
-                elif dp[i - 50][0] == dp[i][0] + 1:
-                    dp[i - 50] = [dp[i - 50][0] + 1, max(dp[i][1], dp[i - 50][1]) + 1]
             if i - j >= 0:
-                pass
+                if dp[i - j][0] > dp[i][0] + 1:
+                    dp[i - j] = list(map(lambda x: x + 1, dp[i]))
+                elif dp[i - j][0] == dp[i][0] + 1:
+                    dp[i - j] = [dp[i - j][0] + 1, max(dp[i][1], dp[i - j][1]) + 1]
+                else:
+                    dp[i - j] =
     return dp[0]
 
 testcase = [
