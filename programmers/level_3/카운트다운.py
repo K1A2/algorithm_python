9ad81934f6ecfa29dd403f61ba8ef3e5b333feeb
K1A2 <jckim0307@gmail.com>
2023-03-02 17:00:12 +0900

    프로그래머스 카운트다운 중간저장

---
+++ programmers/level_3/카운트다운.py
@@ -0,0 +1,21 @@
+def solution(target):
+    dp = [[target - i, target - i] for i in range(target + 1)]
+    for i in range(target, -1, -1):
+        for j in range(20, 0, -1):
+            if i - 50 >= 0:
+                if dp[i - 50][0] > dp[i][0] + 1:
+                    dp[i - 50] = list(map(lambda x: x + 1, dp[i]))
+                elif dp[i - 50][0] == dp[i][0] + 1:
+                    dp[i - 50] = [dp[i - 50][0] + 1, max(dp[i][1], dp[i - 50][1]) + 1]
+            if i - j >= 0:
+                pass
+    return dp[0]
+
+testcase = [
+    ([21], [1,0]),
+    ([58], [2,2]),
+]
+
+if __name__ == '__main__':
+    for idx, t in enumerate(testcase):
+        print(f'{idx}번째: {solution(*t[0]) == t[-1]}')
