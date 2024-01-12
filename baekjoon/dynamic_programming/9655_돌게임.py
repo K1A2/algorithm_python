ef1bc780cddbde0dcb41328176cc3918f112c4f8
K1A2 <jckim0307@gmail.com>
2024-01-12 20:49:07 +0900

    백준 9655 돌게임 풀이

---
+++ baekjoon/dynamic_programming/9655_돌게임.py
@@ -0,0 +1,10 @@
+n = int(input())
+dp = [0] * 1001
+dp[1] = 1
+dp[2] = 2
+for i in range(3, n + 1):
+    dp[i] = min(dp[i - 3], dp[i - 1]) + 1
+if dp[i] % 2 == 0:
+    print('CY')
+else:
+    print('SK')
