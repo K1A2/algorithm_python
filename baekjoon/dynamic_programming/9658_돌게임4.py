6f5e4ccbe9a0efb6ae113c2a8b590b9bb61d606b
K1A2 <jckim0307@gmail.com>
2024-03-12 11:06:49 +0900

    백준 9658 돌게임4 풀이

---
+++ baekjoon/dynamic_programming/9658_돌게임4.py
@@ -0,0 +1,12 @@
+n = int(input())
+dp = [0] * 1001
+dp[2] = dp[4] = 1
+for i in range(5, n + 1):
+    if dp[i - 1] and dp[i - 3] and dp[i - 4]:
+        dp[i] = 0
+    else:
+        dp[i] = 1
+if dp[n] == 1:
+    print('SK')
+else:
+    print('CY')
