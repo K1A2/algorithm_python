fc7b799a3e70371e6adb5e4c82c874437cd46362
K1A2 <jckim0307@gmail.com>
2024-04-28 20:50:23 +0900

    백준 11689 GCD(n, k) = 1 풀이

---
+++ baekjoon/math/11689_GCD.py
@@ -0,0 +1,10 @@
+n = int(input())
+ans = n
+for i in range(2, int(n ** 0.5) + 1):
+    if n % i == 0:
+        ans = ans // i * (i - 1)
+    while n % i == 0:
+        n //= i
+if n != 1:
+    ans = ans // n * (n - 1)
+print(int(ans))
