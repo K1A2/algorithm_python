2a25c58abf73f8bff537d842a92f95435eb498aa
K1A2 <jckim0307@gmail.com>
2024-04-30 10:25:42 +0900

    백준 6549 히스토그램에서 가장 큰 직사각형 풀이

---
+++ baekjoon/segment_tree/6549_히스토그램에서_가장큰_직사각형.py
@@ -0,0 +1,51 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = lambda: sys.stdin.readline()
+
+def init_segment(node, left, right):
+    if left == right:
+        tree[node] = left
+        return
+    mid = (left + right) // 2
+    init_segment(node * 2, left, mid)
+    init_segment(node * 2 + 1, mid + 1, right)
+    tree[node] = tree[node * 2] if value[tree[node * 2]] <= value[tree[node * 2 + 1]] else tree[node * 2 + 1]
+
+def query(node, start, end, left, right):
+    if left > end or right < start:
+        return -1
+    if start <= left and right <= end:
+        return tree[node]
+    mid = (left + right) // 2
+    q1 = query(node * 2, start, end, left, mid)
+    q2 = query(node * 2 + 1, start, end, mid + 1, right)
+    if q1 == -1:
+        return q2
+    if q2 == -1:
+        return q1
+    return q1 if value[q1] < value[q2] else q2
+
+def calculate_area(left, right):
+    idx = query(1, left, right, 0, n - 1)
+    area = (right - left + 1) * value[idx]
+    if left < idx:
+        new_area = calculate_area(left, idx - 1)
+        area = max(area, new_area)
+    if idx < right:
+        new_area = calculate_area(idx + 1, right)
+        area = max(area, new_area)
+    return area
+
+if __name__ == '__main__':
+    while 1:
+        l = list(map(int, input().split()))
+        n = l[0]
+        if n == 0:
+            break
+
+        value = l[1:]
+
+        tree = [-1] * (4 * n)
+        init_segment(1, 0, n - 1)
+
+        print(calculate_area(0, n - 1))
