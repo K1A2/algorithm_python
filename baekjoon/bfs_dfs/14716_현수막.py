8ea4c1055e9f97d454db73110ac6c2d5edb894bf
K1A2 <jckim0307@gmail.com>
2024-06-15 19:51:16 +0900

    백준 14716 현수막 풀이

---
+++ baekjoon/bfs_dfs/14716_현수막.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+input = lambda : sys.stdin.readline()
+n, m = map(int, input().split())
+data = [list(map(int, input().split())) for _ in range(n)]
+
+visited = [[0] * m for _ in range(n)]
+dxy = ((1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1))
+def dfs(x, y):
+    q = deque()
+    q.append((x, y))
+    visited[x][y] = 1
+    while q:
+        x, y = q.popleft()
+        for d in dxy:
+            nx, ny = x + d[0], y + d[1]
+            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
+                if data[nx][ny] == 1:
+                    q.append((nx, ny))
+                    visited[nx][ny] = 1
+
+count = 0
+for i in range(n):
+    for j in range(m):
+        if not visited[i][j] and data[i][j] == 1:
+            count += 1
+            dfs(i, j)
+print(count)
