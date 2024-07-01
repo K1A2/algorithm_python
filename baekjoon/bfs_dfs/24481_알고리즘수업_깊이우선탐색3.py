f5f785f38231fa8e175ad175ce71e634345d9791
K1A2 <jckim0307@gmail.com>
2024-07-02 08:19:33 +0900

    백준 24481 알고리즘수업-깊이우선탐색3 풀이

---
+++ baekjoon/bfs_dfs/24481_알고리즘수업_깊이우선탐색3.py
@@ -0,0 +1,22 @@
+import sys
+sys.setrecursionlimit(10 ** 8)
+
+input = lambda : sys.stdin.readline()
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+depth = [-1] * (n + 1)
+count = 1
+
+def dfs(now_node, count):
+    for next_node in sorted(graph[now_node]):
+        if depth[next_node] == -1:
+            depth[next_node] = count
+            dfs(next_node, count + 1)
+depth[r] = 0
+dfs(r, 1)
+for i in range(1, n + 1):
+    print(depth[i])
