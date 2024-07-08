7cf13b6ab06200912b5824c5cacf97d0e488a4d0
K1A2 <jckim0307@gmail.com>
2024-07-08 11:45:33 +0900

    백준 24445 알고리즘수업-너비우선탐색2 풀이

---
+++ baekjoon/bfs_dfs/24445_알고리즘수업_너비우선탐색2.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+input = lambda : sys.stdin.readline()
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+for i in range(1, n + 1):
+    graph[i] = sorted(graph[i], reverse=True)
+order = [0] * (n + 1)
+count = 1
+order[r] = 1
+
+q = deque()
+q.append(r)
+while q:
+    now = q.popleft()
+    for next_node in graph[now]:
+        if order[next_node] == 0:
+            count += 1
+            order[next_node] = count
+            q.append(next_node)
+
+for i in range(1, n + 1):
+    print(order[i])
