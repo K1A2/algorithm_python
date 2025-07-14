08f540f38d9a47d2b9e4b9b2ec6b498f2a3de033
K1A2 <jckim0307@gmail.com>
2025-07-14 20:41:33 +0900

    백준 17179 케이크 자르기 풀이

---
+++ baekjoon/binary_search/17179_케이크_자르기.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+n, m, l = map(int, input().split())
+data = [int(input()) for _ in range(m)]
+data.append(l)
+
+def check(target, cut):
+    count = 0
+    last_cut = 0
+    for i in range(m + 1):
+        if data[i] - last_cut >= target:
+            count += 1
+            last_cut = data[i]
+    return count > cut
+
+for _ in range(n):
+    c = int(input())
+    low = 0
+    high = l
+    ans = 0
+    while low <= high:
+        mid = (low + high) // 2
+        if check(mid, c):
+            low = mid + 1
+            ans = max(ans, mid)
+        else:
+            high = mid - 1
+    print(ans)
