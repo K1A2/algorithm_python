4aa5aa6a81f01f4182085f5ffe27741970fd6ba3
K1A2 <jckim0307@gmail.com>
2025-08-05 09:30:48 +0900

    백준 16194 카드 구매하기2 풀이

---
+++ baekjoon/dynamic_programming/16194_카드구매하기2.py
@@ -0,0 +1,8 @@
+n = int(input())
+data = [0] + list(map(int, input().split()))
+dp = [0] * (n + 1)
+for i in range(1, n + 1):
+    dp[i] = data[i]
+    for j in range(1, i + 1):
+        dp[i] = min(dp[i], dp[j] + dp[i - j])
+print(dp[-1])
