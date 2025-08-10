de37231edf02925a1c18b122e60a00fd3ad710a9
K1A2 <jckim0307@gmail.com>
2025-08-10 16:20:04 +0900

    백준 4883 삼각 그래프 풀이

---
+++ baekjoon/dynamic_programming/4883_삼각_그래프.py
@@ -0,0 +1,15 @@
+c = 1
+while 1:
+    n = int(input())
+    if n == 0: break
+    graph = [list(map(int, input().split())) for _ in range(n)]
+    graph[0][2] += graph[0][1]
+    graph[1][0] += graph[0][1]
+    graph[1][1] += min(graph[0][1], graph[0][2], graph[1][0])
+    graph[1][2] += min(graph[0][1], graph[0][2], graph[1][1])
+    for i in range(2, n):
+        graph[i][0] += min(graph[i - 1][0], graph[i - 1][1])
+        graph[i][1] += min(graph[i - 1][0], graph[i - 1][1], graph[i - 1][2], graph[i][0])
+        graph[i][2] += min(graph[i - 1][1], graph[i - 1][2], graph[i][1])
+    print(f'{c}. {graph[n - 1][1]}')
+    c += 1
