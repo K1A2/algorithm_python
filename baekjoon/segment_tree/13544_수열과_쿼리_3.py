b098a1c041d0b84f4c3b597e42702d55c00466e9
K1A2 <jckim0307@gmail.com>
2025-09-25 08:54:36 +0900

    백준 13544 수열과 쿼리 3 풀이

---
+++ baekjoon/segment_tree/13544_수열과_쿼리_3.py
@@ -0,0 +1,57 @@
+import sys
+from bisect import bisect_right
+input = sys.stdin.readline
+
+n = int(input())
+data = list(map(int, input().split()))
+
+tree = [[] for _ in range(4 * n)]
+
+
+def merge(node1, node2):
+    idx1 = 0
+    idx2 = 0
+    res = []
+    while idx1 < len(node1) and idx2 < len(node2):
+        if node1[idx1] <= node2[idx2]:
+            res.append(node1[idx1])
+            idx1 += 1
+        else:
+            res.append(node2[idx2])
+            idx2 += 1
+    while idx1 < len(node1):
+        res.append(node1[idx1])
+        idx1 += 1
+    while idx2 < len(node2):
+        res.append(node2[idx2])
+        idx2 += 1
+    return res
+
+
+def init(node, left, right):
+    if left == right:
+        tree[node].append(data[left - 1])
+        return
+    mid = (left + right) // 2
+    init(node * 2, left, mid)
+    init(node * 2 + 1, mid + 1, right)
+    tree[node] = merge(tree[node * 2], tree[node * 2 + 1])
+
+
+def query(node, start, end, left, right, value):
+    if right < start or end < left:
+        return 0
+    if start <= left and right <= end:
+        return len(tree[node]) - bisect_right(tree[node], value)
+    mid = (left + right) // 2
+    return query(node * 2, start, end, left, mid, value) + \
+        query(node * 2 + 1, start, end, mid + 1, right, value)
+
+
+init(1, 1, n)
+
+last_ans = 0
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    last_ans = query(1, a ^ last_ans, b ^ last_ans, 1, n, c ^ last_ans)
+    sys.stdout.write(f'{last_ans}\n')
