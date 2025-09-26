1694076a12c0b5816eafc5abdb264eef0dfe044e
K1A2 <jckim0307@gmail.com>
2025-09-26 14:08:18 +0900

    백준 7469 K번째 수 풀이

---
+++ baekjoon/segment_tree/7469_K번째_수.py
@@ -0,0 +1,63 @@
+import sys
+from bisect import bisect_right
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+
+tree = [[] for _ in range(4 * n)]
+
+
+def merge(node1, node2):
+    idx1 = 0
+    idx2 = 0
+    res = []
+    while idx1 < len(node1) and idx2 < len(node2):
+        if node1[idx1] <= node2[idx2]:
+            res.append(node1[idx1])
+            idx1 += 1
+        else:
+            res.append(node2[idx2])
+            idx2 += 1
+    while idx1 < len(node1):
+        res.append(node1[idx1])
+        idx1 += 1
+    while idx2 < len(node2):
+        res.append(node2[idx2])
+        idx2 += 1
+    return res
+
+
+def init(node, left, right):
+    if left == right:
+        tree[node].append(data[left - 1])
+        return
+    mid = (left + right) // 2
+    init(node * 2, left, mid)
+    init(node * 2 + 1, mid + 1, right)
+    tree[node] = merge(tree[node * 2], tree[node * 2 + 1])
+
+
+def query(node, start, end, left, right, value):
+    if right < start or end < left:
+        return 0
+    if start <= left and right <= end:
+        return bisect_right(tree[node], value)
+    mid = (left + right) // 2
+    return query(node * 2, start, end, left, mid, value) + \
+        query(node * 2 + 1, start, end, mid + 1, right, value)
+
+
+init(1, 1, n)
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    start = -int(1e9)
+    end = int(1e9)
+    while start <= end:
+        mid = (start + end) // 2
+        v = query(1, a, b, 1, n, mid)
+        if v < c:
+            start = mid + 1
+        else:
+            end = mid - 1
+    sys.stdout.write(f'{start}\n')
