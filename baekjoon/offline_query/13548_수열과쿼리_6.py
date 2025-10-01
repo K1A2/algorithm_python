bef0993080c62e7e4c00a841345feda48654ba50
K1A2 <jckim0307@gmail.com>
2025-10-01 09:53:58 +0900

    백준 13548 수열과 쿼리 6 풀이

---
+++ baekjoon/offline_query/13548_수열과쿼리_6.py
@@ -0,0 +1,48 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+data = list(map(int, input().split()))
+m = int(input())
+query = []
+for i in range(m):
+    a, b = map(int, input().split())
+    query.append((i, a - 1, b - 1))
+sqrtn = n ** 0.5
+query.sort(key=lambda x: (x[1] // sqrtn, x[2]))
+
+ans = [''] * m
+left = 0
+right = -1
+numbers = [0] * 100001
+count = [0] * 100001
+max_count = 0
+
+for i, a, b in query:
+    while left < a:
+        if numbers[data[left]] == max_count and count[max_count] == 1:
+            max_count = numbers[data[left]] - 1
+        count[numbers[data[left]]] -= 1
+        numbers[data[left]] -= 1
+        count[numbers[data[left]]] += 1
+        left += 1
+    while left > a:
+        left -= 1
+        count[numbers[data[left]]] -= 1
+        numbers[data[left]] += 1
+        count[numbers[data[left]]] += 1
+        max_count = max(max_count, numbers[data[left]])
+    while right < b:
+        right += 1
+        count[numbers[data[right]]] -= 1
+        numbers[data[right]] += 1
+        count[numbers[data[right]]] += 1
+        max_count = max(max_count, numbers[data[right]])
+    while right > b:
+        if numbers[data[right]] == max_count and count[max_count] == 1:
+            max_count = numbers[data[right]] - 1
+        count[numbers[data[right]]] -= 1
+        numbers[data[right]] -= 1
+        count[numbers[data[right]]] += 1
+        right -= 1
+    ans[i] = str(max_count)
+sys.stdout.write('\n'.join(ans))
