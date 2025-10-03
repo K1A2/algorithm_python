e8db6daf76c7e01701d9850d380b8cbe79f7cfb7
K1A2 <jckim0307@gmail.com>
2025-10-03 10:16:39 +0900

    백준 6515 Frequent values 풀이

---
+++ baekjoon/offline_query/6515_Frequent_values.py
@@ -0,0 +1,50 @@
+import sys
+input = sys.stdin.readline
+while 1:
+    a = input().rstrip()
+    if a == '0': break
+    n, m = map(int, a.split())
+    data = list(map(lambda x: int(x) + 100000, input().split()))
+    query = []
+    for i in range(m):
+        a, b = map(int, input().split())
+        query.append((i, a - 1, b - 1))
+    sqrtn = n ** 0.5
+    query.sort(key=lambda x: (x[1] // sqrtn, x[2]))
+
+    ans = [''] * m
+    left = 0
+    right = -1
+    numbers = [0] * 200010
+    count = [0] * 200010
+    max_count = 0
+
+    for i, a, b in query:
+        while left < a:
+            if numbers[data[left]] == max_count and count[max_count] == 1:
+                max_count = numbers[data[left]] - 1
+            count[numbers[data[left]]] -= 1
+            numbers[data[left]] -= 1
+            count[numbers[data[left]]] += 1
+            left += 1
+        while left > a:
+            left -= 1
+            count[numbers[data[left]]] -= 1
+            numbers[data[left]] += 1
+            count[numbers[data[left]]] += 1
+            max_count = max(max_count, numbers[data[left]])
+        while right < b:
+            right += 1
+            count[numbers[data[right]]] -= 1
+            numbers[data[right]] += 1
+            count[numbers[data[right]]] += 1
+            max_count = max(max_count, numbers[data[right]])
+        while right > b:
+            if numbers[data[right]] == max_count and count[max_count] == 1:
+                max_count = numbers[data[right]] - 1
+            count[numbers[data[right]]] -= 1
+            numbers[data[right]] -= 1
+            count[numbers[data[right]]] += 1
+            right -= 1
+        ans[i] = str(max_count)
+    sys.stdout.write('\n'.join(ans) + '\n')
