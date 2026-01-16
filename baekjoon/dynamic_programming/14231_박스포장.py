291df4c382b72a39404de795911891c8ab8dc310
K1A2 <jckim0307@gmail.com>
2026-01-16 09:10:11 +0900

    백준 14231 박스포장 풀이

---
+++ baekjoon/dynamic_programming/14231_박스포장.py
@@ -0,0 +1,11 @@
+n = int(input())
+data = list(map(int, input().split()))
+dp = [0] * n
+dp[0] = 1
+for i in range(1, n):
+    max_value = 0
+    for j in range(i):
+        if data[j] < data[i] and max_value < dp[j]:
+            max_value = dp[j]
+    dp[i] = max_value + 1
+print(max(dp))
